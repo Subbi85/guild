5598afba24543fd11f85718459eae01f5bf4e570
Subbi85 <lars.schnabel@gmx.de>
2018-07-01 23:43:00 +0200

    .

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Index - guild</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/index.css" />
+</head>
+    <div id="wrapper">
+        <header>
+            <h1>Order and Chaos </h1>
+        </header>
+        <nav> <!-- Nav Bar -->
+            <ul>
+                <li><div class="logo"></div> Home</li>
+                <li><div class="logo"></div>Über uns</li>
+                <li><div class="logo"></div>Rankings</li>
+                <li><div class="logo"></div>Kontakt</li>
+                <li></li>
+            </ul>
+        </nav>
+    </div>
+
+    <div id="js-viewport-breitenanzeiger"></div>
+
+
+
+<body>
+    
+</body>
+</html>
