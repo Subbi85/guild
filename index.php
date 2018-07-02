ba7b48be389c95fdd6910c5fd4752e246954a1e9
Subbi85 <lars.schnabel@gmx.de>
2018-07-02 14:25:30 +0200

    .

--- index.php
+++ index.php
@@ -8,15 +8,6 @@
     <link rel="stylesheet" type="text/css" media="screen" href="css/index.css" />
 </head>
     <div id="wrapper">
-        <header>
-            <div id="banner">
-            
-            </div>
-            <div id="text">
-                <h1>Order and Chaos </h1>
-            </div>
-
-        </header>
         <nav> <!-- Nav Bar -->
             <ul>
                 <li><div class="logo"></div> Home</li>
@@ -26,6 +17,39 @@
                 <li></li>
             </ul>
         </nav>
+        <header>
+            <div id="banner">
+            
+            </div>
+            <div id="text">
+                <h1>Order and Chaos </h1>
+            </div>
+
+        </header>
+
+
+
+    <div id="wahlfeld">
+        <div class="number" id="1">1</div>
+        <div class="number" id="2">2</div>
+        <div class="number" id="3">3</div>
+        <div class="number" id="4">4</div>
+        <div class="number" id="5">5</div>
+        <div class="number" id="6">6</div>
+        <div class="number" id="7">7</div>
+        <div class="number" id="8">8</div>
+        <div class="number" id="9">9</div>
+        <div class="number" id="#">#</div>
+        <div class="number" id="0">0</div>
+        <div class="number" id="ok">OK</div>
+    </div>
+
+    <script>
+
+
+    </script>
+
+
     </div>
 
 
