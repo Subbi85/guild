5cbde59752b0bab1796f9a5de2861203a150b383
Subbi85 <lars.schnabel@gmx.de>
2018-07-11 19:56:16 +0200

    .

--- index.php
+++ index.php
@@ -89,25 +89,33 @@
         <div id="accordionProgress">
   
         <button class="accordionProgress">Smaragdbrüner Alptraum</button>
-        <div class="panelProgress">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            <br><br><a href="">Hier</a> geht es zum Battle.net.</p>
+        <div class="panelProgress" id="nightmare">
+            <p>Der erste Raid der Expansion hat uns nicht so lang aufgehalten. Als Einstiegsraid durchaus angenehm. <br>
+               Die einzigen beiden harten Brocken waren Cenarius und Xavius im hc. Aber auch die konnten uns nicht lang standhalten</p>
         </div>
         <button class="accordionProgress">Prüfung der Tapferkeit</button>
-        <div class="panelProgress">
-            <p>Ein Teil unseres "Gute Seele der Gilde"-Kompetenzteams. Knusper </p>
+        <div class="panelProgress" id="trials">
+            <p>Konnte nur als "Zwischenraid" bzw. "Übergangsraid" angesehen werden. Die Bosse stellten uns sowohl im normalen als auch <br>
+                heroischen Content vor keine unüberwindbaren Herausforderungen. Einzig Helya war zwischendurch ein wenig zickig und wollte sich nicht legen lassen.
+        </p>
         </div>
         <button class="accordionProgress">Nachtfestung</button>
-        <div class="panelProgress">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <div class="panelProgress" id="nighthold">
+            <p>Nach schier endlosen Stunden des Questens und Ruffarmens in einem der schönsten Gebiete der Expansion, konnten wir uns endlich der Nachtfestung widmen. <br>
+            Neben Kuchenessen bei Trillax (bei dem einige Gildenmember instant auf Teilzeitdiät umschalteten wurden) oder dem Brückentanz bei Krosus oder Sternegucken bei Etraeus, hatte die Instanz einiges zu bieten. Nach einigen Schwierigkeiten, wurde die Instanz auch geklärt und Guld verhauen. </p>
         </div>
         <button class="accordionProgress"> Grabmal von Sageras</button>
-        <div class="panelProgress">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <div class="panelProgress" id="tomb">
+            <p>Jetzt wo Gul'dan aufgehalten und Illidan befreit wurde, fokusierten wir uns auf die Zentrale der Legion auf Azeroth. Es ging ins Grabmal. Auch hier hatten wir ein paar Bosse die uns etwas aufgehalten haben. <br>
+                So entdeckten einige von uns bei der Maid ihre Farbenblindheit und bei Harjatan und Herrin Sassz'ine ihre Liebe zu Morlucs und Fischgetier. Als letzte große Herausforderung (besonders im HC) sollte sich Kil'jaeden erweisen. <br>
+                Allerdings musste er uns am Ende aber auch klein beigeben.
+        </p>
         </div>
         <button class="accordionProgress">Antorus, der brennende Thron</button>
-        <div class="panelProgress">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <div class="panelProgress" id="antorus">
+            <p>Im letzten Raid widmeten wir uns auf Argus dem guten Sageras selbst. Eigentlich lief die Instanz ziemlich gut für uns. Bis auf kleinere Problme bei Kin'garoth, der mit seinem Laserstrahl so gern fangen spielte, dem Zirkel der Shivarra und <br>
+                Agrammar, der uns mit seinen Adds und seiner Spezialtechnik zusetzte,stellte uns nur Argus vor eine Herausforderung. Am Ende wurde aber auch dieser überwunden, Azeroth gerettet und das Schwert "bewundert" .
+        </p>
         </div>
     
     </section>
