efc4c05e343d50be08190b50acbc403bbc380660
lars.schnabel@gmx.de <lars.schnabel@gmx.de>
2018-12-06 03:35:24 +0100

    .

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script type="text/javascript" src="script.js"></script>
     <link rel="stylesheet" type="text/css" media="screen" href="css/index.css"/>
-    <link rel="stylesheet" type="text/css" media="screen" href="css/slider.css"/>
+    <link rel="stylesheet" type="text/css" media="screen" href="css/tabs.css"/>
 </head>
 <body>
     <div id="wrapper">
@@ -34,7 +34,7 @@
     <div id="aboutUs">
         <div id="desc"> <!--  CONTAINER -->
             <div id="box-1">
-                <img src="css/img/test.jpg" alt="css/img/test.jpg" width="100%" height="">
+                <img src="css/img/test.jpg" alt="css/img/test.jpg" width="100%" height="600px" margin-left="10px">
             </div>
             <div id="box-2">
                 <h3>Lok'thar Ogar, Freund!</h3>    
@@ -54,8 +54,181 @@
             </div>
         </div> 
     </div>
-    <div id="raids">.<!-- Raids ENDE -->
-        <h3>Rund um unsere Raids</h3>
+    <div id="raids"><!-- Raids -->
+    <div id="wrapper">
+        <h3>Raids:</h3>
+        <div class="tab">
+            <button class="tablinks" onclick="openCity(event, 'bfa')" id="defaultOpen" >BfA</button>
+            <button class="tablinks" onclick="openCity(event, 'legion')" >Legion</button>
+            <button class="tablinks" onclick="openCity(event, 'member')">Raidkader</button>
+            <button class="tablinks" onclick="openCity(event, 'infos')">Allgemeine Infos</button>
+        </div>
+
+        <div id="bfa" class="tabcontent">
+        <span onclick="this.parentElement.style.display='none'" class="topright">&times</span>
+        <h3>Battle for Azeroth</h3>
+        <div id="tab_wrapper"><!-- Tab Wrapper  -->
+            <div id="raid-1"><!-- Raid-1 Box -->
+            <p>
+                Nachdem wir alle den Start ins Add On gemeistert und uns an den "neuen Alltag" gewöhnt haben, öffnete endlich <br>
+                Uldir seine Pforten! Wir waren alle sehr gespannt und waren neugierig was uns erwarten würde. Immerhin gab es einen
+                alten Gott und jede Menge Monster zu verprügeln.
+                <br><br>
+                Der nhc Content konnte uns nicht lange aufhalten und so widmeten wir uns zeitnahm der heroischen Version des <br>
+                Nach ein paar Schwierigkeiten und einiger Zeit der Eingewöhnung gelang es uns schließlich zuerst Zul, dann Mythrax und <br>
+                schlussendlich auch G'huun selbst den Gar aus zu machen und die "Curve" für uns einzustreichen.
+            </p>
+                <div class="skillbar clearfix " data-percent="100%">
+                    <div class="skillbar-title" style="background: #3498db;"><span>Uldir (nhc)</span></div>
+                    <div class="skillbar-bar" style="background: #3498db;"></div>
+                    <div class="skill-bar-percent">8/8</div>
+                </div> <!-- End Skill Bar -->
+
+                <div class="skillbar clearfix " data-percent="100%">
+                    <div class="skillbar-title" style="background: #3498db;"><span>Uldir (hc)</span></div>
+                    <div class="skillbar-bar" style="background: #3498db;"></div>
+                    <div class="skill-bar-percent">8/8</div>
+                </div> <!-- End Skill Bar -->
+            </div> <!-- Raid-1 Box ENDE -->
+            <div id="raid-2"> <!-- Raid-2 Box-->
+            <div class="raidcontainer">
+                <img src="css/img/wappen_middle.png" alt="css/img/wappen_small.png">
+                    <h1>G'huun besiegt!</h1>
+                <div class="color-overlay"></div>
+            </div>
+            </div><!-- Raid-2 Box- ENDE-->
+        </div> <!-- Tab Wrapper ENDE -->
+        </div>
+
+        <div id="legion" class="tabcontent">
+            <span onclick="this.parentElement.style.display='none'" class="topright">&times</span>
+            
+            <h3>Legion</h3>
+            <div id="tab_wrapper"><!-- Tab Wrapper  -->
+            <div id="raid-1"><!-- Raid-1 Box -->
+            
+                <div class="skillbar clearfix " data-percent="100%">
+                    <div class="skillbar-title" style="background: #3498db;"><span>Nighthold (nhc)</span></div>
+                    <div class="skillbar-bar" style="background: #3498db;"></div>
+                    <div class="skill-bar-percent">8/8</div>
+                </div> <!-- End Skill Bar -->
+
+                <div class="skillbar clearfix " data-percent="100%">
+                    <div class="skillbar-title" style="background: #3498db;"><span>Nighthold (hc)</span></div>
+                    <div class="skillbar-bar" style="background: #3498db;"></div>
+                    <div class="skill-bar-percent">8/8</div>
+                </div>  <br><!-- End Skill Bar -->
+
+                <div class="skillbar clearfix " data-percent="100%">
+                    <div class="skillbar-title" style="background: #3498db;"><span>Trials of Valor (nhc)</span></div>
+                    <div class="skillbar-bar" style="background: #3498db;"></div>
+                    <div class="skill-bar-percent">3/3</div>
+                </div> <!-- End Skill Bar -->
+
+                <div class="skillbar clearfix " data-percent="100%">
+                    <div class="skillbar-title" style="background: #3498db;"><span>Trials of Valor (hc)</span></div>
+                    <div class="skillbar-bar" style="background: #3498db;"></div>
+                    <div class="skill-bar-percent">3/3</div>
+                </div> <br><!-- End Skill Bar -->
+
+                        <div class="skillbar clearfix " data-percent="100%">
+                    <div class="skillbar-title" style="background: #3498db;"><span>Nighthold (nhc)</span></div>
+                    <div class="skillbar-bar" style="background: #3498db;"></div>
+                    <div class="skill-bar-percent">8/8</div>
+                </div> <!-- End Skill Bar -->
+
+                <div class="skillbar clearfix " data-percent="100%">
+                    <div class="skillbar-title" style="background: #3498db;"><span>Nighthold (hc)</span></div>
+                    <div class="skillbar-bar" style="background: #3498db;"></div>
+                    <div class="skill-bar-percent">8/8</div>
+                </div> <br> <!-- End Skill Bar -->
+
+                <div class="skillbar clearfix " data-percent="100%">
+                    <div class="skillbar-title" style="background: #3498db;"><span>ToS (nhc)</span></div>
+                    <div class="skillbar-bar" style="background: #3498db;"></div>
+                    <div class="skill-bar-percent">8/8</div>
+                </div> <!-- End Skill Bar -->
+
+                <div class="skillbar clearfix " data-percent="100%">
+                    <div class="skillbar-title" style="background: #3498db;"><span>ToS (hc)</span></div>
+                    <div class="skillbar-bar" style="background: #3498db;"></div>
+                    <div class="skill-bar-percent">8/8</div>
+                </div> <br><!-- End Skill Bar -->
+
+                <div class="skillbar clearfix " data-percent="100%">
+                    <div class="skillbar-title" style="background: #3498db;"><span>Antorus (nhc)</span></div>
+                    <div class="skillbar-bar" style="background: #3498db;"></div>
+                    <div class="skill-bar-percent">8/8</div>
+                </div> <!-- End Skill Bar -->
+
+                <div class="skillbar clearfix " data-percent="100%">
+                    <div class="skillbar-title" style="background: #3498db;"><span>Antorus (hc)</span></div>
+                    <div class="skillbar-bar" style="background: #3498db;"></div>
+                    <div class="skill-bar-percent">8/8</div>
+                </div> <br><!-- End Skill Bar -->
+            </div> <!-- Raid-1 Box ENDE -->
+        <div id="raid-2"> <!-- Raid-2 Box-->
+            <div class="slider">
+                <div class="slide"><p>Nightmare: CLEAR</p></div>
+                <div class="slide"><p>Nighthold: CLEAR</p></div>
+                <div class="slide"><p>Grabmahl des Sageras: CLEAR</p></div>
+                <div class="slide"><p>Antorus: CLEAR</p></div>  
+            </div>
+        </div><!-- Raid-2 Box- ENDE-->
+
+        </div> <!-- Tab Wrapper ENDE -->
+    </div>
+        <div id="member" class="tabcontent">
+            <span onclick="this.parentElement.style.display='none'" class="topright">&times</span>
+            <h3>Raidkader</h3>
+            <p>Tokyo is the capital of Japan.</p>
+        </div>
+
+        <div id="infos" class="tabcontent">
+            <span onclick="this.parentElement.style.display='none'" class="topright">&times</span>
+            <h3>Raidkader</h3>
+            <p>Allgemeine Informationen zu unseren Raids</p>
+        </div>
+    </div>
+    </div> <!-- Raids ENDE -->
+
+    <div id="mythic"> <!-- Mythic Plus -->
+
+    <h3>Mythic+ Instanzen </h3>
+    <div class="affix" id="one"></div>
+    <div id="t_one"></div>
+    <div class="affix" id="two"></div>
+    <div id="t_two"></div>
+    <div class="affix" id="three"></div>
+    <div id="t_three"></div>
+    <div class="affix" id="four"></div>
+    <div id="t_four"></div>
+
+    <div class="tooltip" id="one">Hover over me
+        <span class="tooltiptext" id="t_one">Tooltip text</span>
+    </div>
+
+    <h2>Right Tooltip w/ Left Arrow</h2>
+    <p>
+        Jetzt ist sind die Affixe ja schon gut und schön, aber das sagt ja nichts über unsere Gilde. Kommen wir nun also zu unseren <br>
+        Top 10 Membern, die sich in der aktuellen Season in die Schlacht gestürzt haben.
+    </p>
+    <table>
+        <tr>
+            <th>Platz</th><th>Name</th><th>Klasse</th><th>Score</th>
+        </tr>
+        <?php
+        for ($i=0; $i<10; $i++){
+        echo "<tr>
+            <td>1</td><td>Kreischi</td><td>Schamane</td><td>1030</td>
+            </tr>";
+            }
+        ?>        
+        </table> 
+    </div> <!-- Mythic Plus ENDE -->
+
+    <div id="sandbox">
+    <h3>Rund um unsere Raids</h3>
         <p> Hier zeigen wir dir unseren Raidprogress der vergangenen Raidinstanzen. Aktiv gemeinsam raiden gehen wir <br>
             wie gesagt seit Nachtfestung in Legion.
         </p>
@@ -64,39 +237,7 @@
             <a href="https://raider.io/guilds/eu/anetheron/Order%20and%20Chaos" target="_blank">Raider.io</a> 
             <a href="https://www.warcraftlogs.com/guild/calendar/304372/" target="_blank"> WarcraftLogs </a>
         </p>
-    <!-- Nachtfestung
-        <div class="con">
-            <h1>Clear! <br>
-                Gul'dan plattgemacht</h1>
-            <div class="col-overlay"></div>
-        </div> -->
- 
-    <!-- Slider -->
-        <div class="slider">
-            <div class="slide"><p>Nightmare: CLEAR</p></div>
-            <div class="slide"><p>Nighthold: CLEAR</p></div>
-            <div class="slide"><p>Grabmahl des Sageras: CLEAR</p></div>
-            <div class="slide"><p>Antorus: CLEAR</p></div>  
-        </div>
-    <!-- Slider ENDE-->
-
-
-    </div> <!-- Raids ENDE -->
-
-    <div id="mythic"> <!-- Mythic Plus -->.
-        <h3>Mythic+ Instanzen </h3>
-        <div class="affix" id="one"></div>
-        <div id="t_one"></div>
-        <div class="affix" id="two"></div>
-        <div id="t_two"></div>
-        <div class="affix" id="three"></div>
-        <div id="t_three"></div>
-        <div class="affix" id="four"></div>
-        <div id="t_four"></div>
-
-
-    </div> <!-- Mythic Plus ENDE -->
-
+    </div>
     </div> <!-- Ende Wrapper -->
 
     <script>
@@ -106,6 +247,33 @@
             $('ul').toggleClass('active');
         });
     });
+
+        // Skillbar Animations
+    $(document).ready(function(){
+        $('.skillbar').each(function(){
+            $(this).find('.skillbar-bar').animate({
+                width:$(this).attr('data-percent')
+            },3000);
+        });
+    });
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
     </script>
 
 </body>
--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 guildMembers();
 affixe();
-progression();
+
+
 
 function guildMembers(){                	/*Erstellen der Memberliste für weitere Funktionen */
 var guildRequest = new XMLHttpRequest();
+
 guildRequest.open('GET', 'https://eu.api.battle.net/wow/guild/Anetheron/Order%20and%20Chaos?fields=members&locale=en_GB&apikey=2z8d96ypab8zbed7nrbz29a3uqxskz5u')
 guildRequest.onload=function(){
     var test = JSON.parse(guildRequest.responseText);
-       console.log(test); 
     var member= [];
     for (i=0; i<test.members.length; i++){
         var rank = test.members[i].rank;
@@ -17,42 +18,51 @@ guildRequest.onload=function(){
             member.push(test.members[i].character["class"]);
         }
     }
-    console.log(member);
-    console.log(test);
     for(i=0; i<member.length; i=i+3){
         var name = member[i];
         var realm = member[i+1];
         var classes = member[i+2]
-        var final = [];
-        final.push(getScore(name, realm, classes));
+        getScore(name, realm, classes);
         }
     }
+    console.log("ENDE");
     guildRequest.send();
 }
-// Ränge: 1,2-> offitwinks ,3,5                SO GUT WIE FERTIG!!!!!
 
 function getScore(name, realm, classes){           /* M+ Scores der Member */
-
-    // name = Tbow;
-    // realm = Terenas;
     var scores = [];
-    var final = [];
     var blizzRequest = new XMLHttpRequest();
+
     blizzRequest.open('GET', 'https://raider.io/api/v1/characters/profile?region=eu&realm='+realm+'&name='+name+'&fields=mythic_plus_scores')
     blizzRequest.onload=function(){
 
     var score = JSON.parse(blizzRequest.responseText);
     var current = score.mythic_plus_scores.all;
     
-    scores.push(name);
-    scores.push(realm);
-    scores.push(current);
-    scores.push(classes);
-    
-    console.log(scores);
+        scores.push(name);
+        scores.push(realm);
+        scores.push(current);
+        scores.push(classes);
+        console.log(scores);
+        document.getElementById("one").innerHTML=scores[0]+scores[2];
+        document.getElementById("t_one").innerHTML=scores[3];
     }
     blizzRequest.send(scores);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function affixe(){                          /* Affixe der aktuellen Woche */
 var blizzRequest = new XMLHttpRequest();
@@ -60,15 +70,12 @@ var affixes = [];
 blizzRequest.open('GET', 'https://raider.io/api/v1/mythic-plus/affixes?region=EU&locale=de')
 blizzRequest.onload=function(){
     var test = JSON.parse(blizzRequest.responseText);
-    console.log(test);
     for(i=0; i<test.affix_details.length; i++){
         var affix = test.affix_details[i].name;
         var detail = test.affix_details[i].description;
-
         affixes.push(affix);
         affixes.push(detail);
     }
-    console.log(affixes);
     document.getElementById("one").innerHTML=affixes[0];
     document.getElementById("t_one").innerHTML=affixes[1]; //Beschreibung
     document.getElementById("two").innerHTML=affixes[2];
@@ -76,18 +83,17 @@ blizzRequest.onload=function(){
     document.getElementById("three").innerHTML=affixes[4];
     document.getElementById("t_three").innerHTML=affixes[5]; // Beschreibung 3
     document.getElementById("four").innerHTML=affixes[6];
-    document.getElementById("t_four").innerHTML=affixes[7]; // Beschreibung 3
+    document.getElementById("t_four").innerHTML=affixes[7]; // Beschreibung 4
 
 }  
 blizzRequest.send();
-}                                           // AFFIXE ENDE //
+}                                           
+// AFFIXE ENDE //
 
 function progression(){
-/* Arrays für die entsprechenden Raids */
 var data = [];
 //var data = [11, 11, 11, 7, 7, 7, 10,10,10, 9, 9, 9, 3, 3, 3];   < Datensicherung der Dungeons aus Legion
 var blizzRequest = new XMLHttpRequest();
-
 blizzRequest.open('GET', 'https://raider.io/api/v1/guilds/profile?region=eu&realm=Anetheron&name=Order%20and%20Chaos&fields=raid_progression')
 // https://raider.io/api/v1/guilds/profile?region=EU&realm=Anetheron&name=Order%20and%20Chaos&fields=raid_progression%2C%20raid_rankings
 blizzRequest.onload=function(){
@@ -116,28 +122,8 @@ blizzRequest.onload=function(){
     data.push(test.raid_progression[raid].total_bosses);
     data.push(test.raid_progression[raid].normal_bosses_killed);
     data.push(test.raid_progression[raid].heroic_bosses_killed);
-
-    //var x = document.getElementById("bosses").offsetWidth;
     }
-   // document.getElementById("enm_normal").style.width = 500+"px";
-   fillIn(data, x);
 }
 blizzRequest.send();
 }
  
-function fillIn(data, x){
-        document.getElementById("ant_normal").style.width = (x/data[0])*data[1]+"px";
-        document.getElementById("ant_heroic").style.width = (x/data[0])*data[2]+"px";
-
-        document.getElementById("enm_normal").style.width = (x/data[3])*data[4]+"px";
-        document.getElementById("enm_heroic").style.width = (x/data[3])*data[5]+"px";
-
-        document.getElementById("hold_normal").style.width = (x/data[6])*data[7]+"px";
-        document.getElementById("hold_heroic").style.width = (x/data[6])*data[8]+"px";
-
-        document.getElementById("tomb_normal").style.width = (x/data[9])*data[10]+"px";
-        document.getElementById("tomb_heroic").style.width = (x/data[9])*data[11]+"px";
-
-        document.getElementById("trials_normal").style.width = (x/data[12])*data[13]+"px";
-        document.getElementById("trials_heroic").style.width = (x/data[12])*data[14]+"px";
-}
