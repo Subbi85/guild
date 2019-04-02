d8e9143678d69186c364b3923165b2898a4d5dfd
lars.schnabel@gmx.de <lars.schnabel@gmx.de>
2019-04-02 16:40:26 +0200

    .

--- index.php
+++ index.php
@@ -1,25 +1,19 @@
 <?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:oac-db.database.windows.net,1433; Database = guild", "Subbi", "{Kalender85}");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
 
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "Subbi@oac-db", "pwd" => "{Kalender85}", "Database" => "guild", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:oac-db.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+//DB Connection
+$conn = new PDO("sqlsrv:server = tcp:oac-db.database.windows.net,1433; Database = guild", "Subbi", "{Kalender85}");
 
-echo "test";
+//Variablen beschaffen
+$crmProdid = $_POST["terminname"];
+$ProductName = $_POST["terminbeschreibung"];
+$ProductNumber =$_POST["datum"];
 
-//Insert Statement
+//Statement
+$insert= $pdo->prepare("    INSERT INTO crmProducts (crmProdid, ProductName, ProductNumber)
+                            VALUES
+                            (:crmProducts, :ProductName, :ProductNumber)
+                            ");
+$result = $insert->execute(array('crmProducts' => $crmProducts, 'ProductName'=> $ProductName, 'ProductNumber'=> $ProductNumber));
 
-
-//ENDE Insert
-
-
+echo "Termin angelegt";
 ?>
