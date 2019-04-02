5184d618d15bc16c381eb83152f61ae1a071a93d
lars.schnabel@gmx.de <lars.schnabel@gmx.de>
2019-04-02 12:38:21 +0200

    .

--- index.php
+++ index.php
@@ -27,6 +27,9 @@ $insert = $conn->prepare("  INSERT INTO crmProducts (crmProdid, ProductName, Pro
                             ('12223', 'kekse', 'schokokekse')";
 $insertReview = sqlsrv_query($conn, $insert);
 
+if($insertReview == FALSE)
+    die(FormatErrors( sqlsrv_errors()));
+
 
 sqlsrv_free_stmt($insertReview);
 //ENDE Insert
