34629092924133c3f4b930e080fcc98f2eda0f4b
lars.schnabel@gmx.de <lars.schnabel@gmx.de>
2019-04-02 16:46:11 +0200

    .

--- index.php
+++ index.php
@@ -3,7 +3,10 @@
 //DB Connection
 $conn = new PDO("sqlsrv:server = tcp:oac-db.database.windows.net,1433; Database = guild", "Subbi", "{Kalender85}");
 
-
+//Variablen beschaffen
+$crmProdid = "122352";
+$ProductName = "kekse";
+$ProductNumber ="Vanillekekse";
 
 //Statement
 $insert= $conn->prepare("    INSERT INTO crmProducts (crmProdid, ProductName, ProductNumber)
