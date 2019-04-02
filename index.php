c3b8510c2f118853c9900e9fd4925cb52196e619
lars.schnabel@gmx.de <lars.schnabel@gmx.de>
2019-04-02 10:42:05 +0200

    .

--- index.php
+++ index.php
@@ -22,13 +22,19 @@ $connectionInfo = array(
 $serverName = "tcp:oac-db.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
+$insert="   INSERT INTO dbo.crmProducts(crmProdid, ProductName, ProductNumber )
+            VALUES
+            (123456666489 ,'NewerColor' ,1121565 )
+        ";
+
+
+$result = sqlsrv_query($conn, $insert);
+
+
 $statement = "  SELECT *
                 FROM  dbo.crmProducts";
 
 $result = sqlsrv_query($conn, $sql);
-
-
-
 
 //Wenn Result leer ist
 if ($result == FALSE){
