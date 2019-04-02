d835a556239e1ca968bdea478c4d2b594ec5de84
lars.schnabel@gmx.de <lars.schnabel@gmx.de>
2019-04-02 12:15:54 +0200

    .

--- index.php
+++ index.php
@@ -3,24 +3,17 @@
 try {
     $conn = new PDO("sqlsrv:server = tcp:oac-db.database.windows.net,1433; Database = guild", "Subbi", "{Kalender85}");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "die Verbindung steht";
 }
-//Verbindungsprüfung
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array(
-                    "UID" => "Subbi@oac-db", 
-                    "pwd" => "{Kalender85}", 
-                    "Database" => "guild",
-                    "LoginTimeout" => 30, "Encrypt" => 1, 
-                    "TrustServerCertificate" => 0);
-
+$connectionInfo = array("UID" => "Subbi@oac-db", "pwd" => "{Kalender85}", "Database" => "guild", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:oac-db.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
+
 
 //Insert Statement
 $crmProdid=12234;
