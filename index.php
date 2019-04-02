c0b22ad2faaa80d175e559f9ac8fcb84d665b153
lars.schnabel@gmx.de <lars.schnabel@gmx.de>
2019-04-02 18:01:31 +0200

    .

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 //DB Connection
 $conn = new PDO("sqlsrv:server = tcp:oac-db.database.windows.net,1433; Database = guild", "Subbi", "{Kalender85}");
 
@@ -9,11 +8,22 @@ $ProductName = "kekse";
 $ProductNumber ="Vanillekekse";
 
 //Statement
-$insert= $conn->prepare("    INSERT INTO crmProducts (crmProdid, ProductName, ProductNumber)
+/*$insert= $conn->prepare("   INSERT INTO crmProducts (crmProdid, ProductName, ProductNumber)
                             VALUES
                             (:crmProdid, :ProductName, :ProductNumber)
                             ");
 $result = $insert->execute(array('crmProdid' => $crmProdid, 'ProductName'=> $ProductName, 'ProductNumber'=> $ProductNumber));
+*/
+
+$select = $conn->("SELECT * FROM crmProduct");
+$result = $select->execute();s
+
+$result = mysqli_query( $conn, $sql);
+if (!$result )
+{
+  die('Ungültige Abfrage: '.mysqli_error());
+}
+
 
 echo "Termin angelegt";
 ?>
