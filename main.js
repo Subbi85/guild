cc6c787a2e4fcf9f6070fb8e3e8cfd939fc1b29c
lars.schnabel@gmx.de <lars.schnabel@gmx.de>
2019-05-02 11:41:03 +0200

    .

--- main.js
+++ main.js
@@ -9,9 +9,9 @@ let tabellenBauen=(data)=>{
     //Neue DOM-Elemente aufbauen
     data.forEach(element => {
         console.log(element.charname);
-        console.log(elment.role);
+        console.log(element.role);
 
-        
+
         let neuesTr = document.createElement("tr");
         //Charname
         let neueTd = document.createElement("td");
