e833800f68358eb49c2cbe97e0dfb58ffe7d94ef
lars.schnabel@gmx.de <lars.schnabel@gmx.de>
2019-05-02 11:06:54 +0200

    .

--- index.php
+++ index.php
@@ -194,7 +194,7 @@
                     member.onreadystatechange = function() {
                         if (this.readyState == 4 && this.status == 200) {
                             let data = JSON.parse(this.responseText);
-                            
+                            console.log(data);
                             tabellenBauen(data);
                         }
                 }
@@ -257,10 +257,50 @@
     </div>
 
 <script>
+     $(document).ready(function() {
+        $('.progress .progress-bar').css("width",
+            function() {
+                return $(this).attr("aria-valuenow") + "%";
+            }
+        )
 
+        $('.skillbar').each(function(){
+            $(this).find('.skillbar-bar').animate({
+                width:$(this).attr('data-percent')
+            },3000);
+        });
+    });
+
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
+    }
     
+    document.getElementById("defaultOpen").click();
+    function openTab(evt, tabName) {
+        // Declare all variables
+        var i, tabcontent, tablinks;
 
+        // Get all elements with class="tabcontent" and hide them
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
 
+        // Get all elements with class="tablinks" and remove the class "active"
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+
+        // Show the current tab, and add an "active" class to the button that opened the tab
+        document.getElementById(tabName).style.display = "block";
+        evt.currentTarget.className += " active";
+    }
 </script>
 </body>
 </html>
--- main.js
+++ main.js
@@ -4,47 +4,4 @@ let tabellenBauen=(data)=>{
     console.log(data);
 }
 
-function openTab(evt, tabName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
-
-$(document).ready(function() {
-    $('.progress .progress-bar').css("width",
-        function() {
-            return $(this).attr("aria-valuenow") + "%";
-        }
-    )
-
-    $('.skillbar').each(function(){
-        $(this).find('.skillbar-bar').animate({
-            width:$(this).attr('data-percent')
-        },3000);
-    });
-});
-
-document.getElementById("defaultOpen").click();
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
