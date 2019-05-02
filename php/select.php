eb89ba5338d7bfeab2908bb44b4cf44589e309af
lars.schnabel@gmx.de <lars.schnabel@gmx.de>
2019-05-02 09:46:19 +0200

    .

--- php/select.php
+++ php/select.php
@@ -1,8 +1,6 @@
 <?php
 $pdo = new PDO("sqlsrv:server = tcp:oac-db.database.windows.net,1433; Database = guild", "Subbi", "{Kalender85}");
-
-
-$sql = " SELECT m.charname, m.realm, c.name as classname
+$sql = " SELECT m.charname, m.realm, c.name as classname, r.role
          FROM members as m
          INNER JOIN roles as r
          ON m.roleId = r.id
