66039ed6f296e69d102e9ae802f946efc0131f15
lars.schnabel@gmx.de <lars.schnabel@gmx.de>
2019-05-06 14:15:10 +0200

    .

--- job.php
+++ job.php
@@ -54,8 +54,10 @@
             //Anpassen des "Problemrealms"
             if(data.realm ==="Guldan")
                 data.realm==="Gul'dan";
+            if(data.charname ==="Corruption")
+                data.charname==="Corruptìon";
             //AJAX-Syntax
-            blizzRequest.open('GET', 'https://raider.io/api/v1/characters/profile?region=eu&realm='+realm+'&name='+name+'&fields=mythic_plus_scores')
+            blizzRequest.open('GET', 'https://raider.io/api/v1/characters/profile?region=eu&realm='+data.realm+'&name='+data.charname+'&fields=mythic_plus_scores')
             blizzRequest.onload=function(){
                 var score = JSON.parse(blizzRequest.responseText);
                 var current = score.mythic_plus_scores.all;
