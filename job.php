7c3286c16124b2c64afe4fb772e2e3e26bee0c04
lars.schnabel@gmx.de <lars.schnabel@gmx.de>
2019-05-06 13:57:29 +0200

    -

--- job.php
+++ job.php
@@ -24,11 +24,7 @@
                 console.log({data});
                 console.log(data.length);
                 console.log(data[10].charname);
-                let daten =[];
-                for (let i=0; i<data.length; i++){
-                    daten.push(data[i]);
-                }
-                console.log(daten);
+                write(data);
             }
         }
         member.open("GET", "php/select.php", true);
@@ -38,27 +34,21 @@
         //Raider.io Scores abrufen
 
 
-        var text =  '{ "member" : [' 
+        var text =  '{ "member" : [';
 
+        let write =(data)=>{
+            console.log({data});
 
-
-        let write =(charname, realm, score)=>{
-            for (let i=0; i<=10; i++){
-                text+= '{ "Charname":"'+charname+'" , "realm":"'+realm+'" ,"score":'+score+' }' 
-                if(i==10){
-
-                }else{
-                    text+= ',';                    
-                }
-
+/*
+                text+= '{ "Charname":"'+charname+'" , "realm":"'+realm+'"}';
             }
             text +=  ' ]}';            
         }   
-        
-        write("Kreischi", "Nathrezim", 1555);
+  */      
+    
 
-        console.log(text);
-        var obj = JSON.parse(text);
+        console.log({text});
+        //var obj = JSON.parse(text);
         console.log({obj});
 
 
