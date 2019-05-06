e5b46c46814088196bc96e775bea55b89922b1d9
lars.schnabel@gmx.de <lars.schnabel@gmx.de>
2019-05-06 13:36:20 +0200

    .

--- job.php
+++ job.php
@@ -21,7 +21,7 @@
         member.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
                 let data = JSON.parse(this.responseText);
-                console.log(data);
+                console.log({data});
                 console.log(data.length);
                 console.log(data[10].charname);
                 let daten =[];
