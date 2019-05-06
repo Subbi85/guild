24e49864d438bde20904b73139ac0315a56cdc3f
lars.schnabel@gmx.de <lars.schnabel@gmx.de>
2019-05-06 11:55:25 +0200

    .

--- job.php
+++ job.php
@@ -26,6 +26,7 @@
                 for (let i=0; i<data.length; i++){
                     daten.push(data[i]);
                 }
+                console.log(daten);
             }
         }
         member.open("GET", "php/select.php", true);
