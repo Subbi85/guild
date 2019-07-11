99691fa87e1af13b76b23fb02a4e0f027f5271bb
lars.schnabel@gmx.de <lars.schnabel@gmx.de>
2019-07-11 15:06:00 +0200

    .

---
+++ grid.js
@@ -0,0 +1,26 @@
+"use strict";
+
+
+//Laden der Bilder bei Seitenaufruf
+window.onload=()=>{
+    let boxList = document.querySelectorAll('.box');
+    console.log(boxList);
+    for(let i=0; i<boxList.length; i++){
+        createImage(i);
+    }
+}
+
+//Erstellen der BildContainer für die Gallerie
+let createImage=(index)=>{
+    let box = document.getElementById('box-'+index);
+    let newImg= document.createElement('img');
+    newImg.classList.add('limiter')
+    newImg.setAttribute('src', 'pic1.png')
+    if(index <=3 || index >=6){        
+        newImg.setAttribute('width', '300px')
+    }else{
+        newImg.setAttribute('width', '600px')
+    }
+    newImg.setAttribute('height', '300px');
+    box.appendChild(newImg);        
+}
