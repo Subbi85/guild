ff0ada952c2873060a39e5924817606f7005fe1f
lars.schnabel@gmx.de <lars.schnabel@gmx.de>
2019-08-11 20:33:39 +0200

    .

--- js/scores.js
+++ js/scores.js
@@ -124,10 +124,10 @@ let createDivs=(obj)=>{
 let getDetails=(id)=>{
     console.log(id);
     let nodeList = document.querySelectorAll('.score_tr');
-    console.log(nodeList);
+    console.log(+nodeList[id].children[1].innerHTML);
     //AJAX Call zu Raider.io
     var blizzRequest = new XMLHttpRequest();
-    blizzRequest.open('GET', 'https://raider.io/api/v1/characters/profile?region=eu&realm='+nodeList[id].children[3].innerHTML+'&name='+nodeList[id].children[2].innerHTML+'&fields=mythic_plus_scores');
+    blizzRequest.open('GET', 'https://raider.io/api/v1/characters/profile?region=eu&realm='+nodeList[id].children[1].innerHTML+'&name='+nodeList[id].children[2].innerHTML+'&fields=mythic_plus_scores');
     blizzRequest.onload=function(){
         var score = JSON.parse(blizzRequest.responseText);
         console.log(score);
