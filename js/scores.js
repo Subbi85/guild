8862a99ddbeb4f2c1161506e19fc3713f72ad430
lars.schnabel@gmx.de <lars.schnabel@gmx.de>
2020-01-05 15:26:16 +0100

    .

--- js/scores.js
+++ js/scores.js
@@ -22,18 +22,17 @@ let getScores=(data)=>{
     console.log(data[5]);
     if (data.realm ==="Guldan")
         data.realm="Gul'dan";
-
-    console.log("https://raider.io/api/v1/characters/profile?region=eu&realm="+data.realm+"&name="+data.charname+"&fields=mythic_plus_scores");
     //AJAX-Syntax
-    /*
-    var iniRequest = new XMLHttpRequest();
-    iniRequest.open('GET', 'https://raider.io/api/v1/characters/profile?region=eu&realm='+data.realm+'&name='+data.charname+'&fields=mythic_plus_scores')
-    iniRequest.onload=function(){
-        var score = JSON.parse(iniRequest.responseText);
-        var current = score.mythic_plus_scores.all;
-        text= createJSON(score, current);
-    }                
-    iniRequest.send(); */
+    for (let i = 0; i< data.length; i++){
+        var iniRequest = new XMLHttpRequest();
+        iniRequest.open('GET', 'https://raider.io/api/v1/characters/profile?region=eu&realm='+data.realm[i]+'&name='+data.charname[i]+'&fields=mythic_plus_scores')
+        iniRequest.onload=function(){
+            var score = JSON.parse(iniRequest.responseText);
+            var current = score.mythic_plus_scores.all;
+            text= createJSON(score, current);
+        }                
+        iniRequest.send();
+    }
 }
 
 //Aufbau JSON
